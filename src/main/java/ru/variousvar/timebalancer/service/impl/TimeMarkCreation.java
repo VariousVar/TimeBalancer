418b8d9641f1bfcef6c0a7f39f846a76eaddda0d
Anatoly Sibiryatko <a.sibiryatko@gmail.com>
2017-03-18 22:26:39 +0300

    Fix aspect pointcuts.

--- src/main/java/ru/variousvar/timebalancer/service/impl/TimeMarkCreation.java
+++ src/main/java/ru/variousvar/timebalancer/service/impl/TimeMarkCreation.java
@@ -1,14 +1,10 @@
 package ru.variousvar.timebalancer.service.impl;
 
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 import ru.variousvar.timebalancer.entity.Timing;
 import ru.variousvar.timebalancer.exception.ConcurrentMarkInsertion;
 
-import java.time.Instant;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -17,22 +13,16 @@ import java.util.concurrent.ConcurrentHashMap;
 public class TimeMarkCreation {
     private Set<Long> timings = ConcurrentHashMap.newKeySet();
 
-    @Before("execution(* ru.variousvar.timebalancer.service.MarkService.getLast(ru.variousvar.timebalancer.entity.Timing)) && args(timing))")
-    public void beforeLastMarkRetrieving(Timing timing) {
-        System.out.println("check timing lock");
+    @Before(value = "execution(* ru.variousvar.timebalancer.service.MarkService.createTimeMark*(ru.variousvar.timebalancer.entity.Timing, ..)) && args(timing, ..))")
+    public void beforeLastMarkRetrievingNow(Timing timing) {
         if (timings.contains(timing.getId()))
-            throw new ConcurrentMarkInsertion("Timing '" + timing.getId() + "' already updated by someone");
-    }
+            throw new ConcurrentMarkInsertion("Timing '" + timing.getId() + "' have been updating by someone");
 
-    @AfterReturning("execution(* ru.variousvar.timebalancer.service.MarkService.getLast(ru.variousvar.timebalancer.entity.Timing)) && args(timing))")
-    public void lockTiming(Timing timing) {
-        System.out.println("lock timing");
         timings.add(timing.getId());
     }
 
-    @After(value = "execution (* ru.variousvar.timebalancer.service.MarkService.createTimeMarkFromDate(ru.variousvar.timebalancer.entity.Timing, java.time.Instant)) && args(timing, date))",
-            argNames = "timing,date")
-    public void unlockTiming(Timing timing, Instant date) {
-        System.out.println("after create");
+    @After(value = "execution (* ru.variousvar.timebalancer.service.MarkService.createTimeMark*(ru.variousvar.timebalancer.entity.Timing, ..)) && args(timing, ..))")
+    public void unlockTiming(Timing timing) {
+        timings.remove(timing.getId());
     }
 }
