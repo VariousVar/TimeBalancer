77d2df4f8fc9b1c5835b24ded34c5288e21f7093
Anatoly Sibiryatko <a.sibiryatko@gmail.com>
2017-11-19 19:38:43 +0300

    Fix resource mapping.

--- src/main/java/ru/variousvar/timebalancer/config/WebMvcConfiguration.java
+++ src/main/java/ru/variousvar/timebalancer/config/WebMvcConfiguration.java
@@ -1,13 +1,11 @@
 package ru.variousvar.timebalancer.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
-@EnableWebMvc
 public class WebMvcConfiguration extends WebMvcConfigurationSupport {
 
     @Override
